f2d64065d08f574b79d11622b7c856162b5c3ab5
jeswanth <jachu.k1212@gmail.com>
2024-11-08 13:50:53 +0530

    CarVilla

--- carvilla/src/App.js
+++ carvilla/src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import HomeSection from './Components/HomeSection';
-// import ServiceSection from './Components/ServiceSection';
+import ServiceSection from './Components/ServiceSection';
 
 function App() {
   return (
     <div className="App">
       <HomeSection />
-      {/* <ServiceSection /> */}
+      <ServiceSection />
     </div>
   );
 }
