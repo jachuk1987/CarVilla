3b74b103dfa30de3683eee03b1192871eafc8d93
jeswanth <jachu.k1212@gmail.com>
2024-11-09 09:11:55 +0530

    CarVilla

--- carvilla/src/Components/ServiceSection.jsx
+++ carvilla/src/Components/ServiceSection.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // Import your icon images
 import carIcon from '../images/car-icon.png';
-import repairIcon from '../assets/images/repair-icon.png';
+import repairIcon from '../images/repair-icon.png';
 import insuranceIcon from '../assets/images/insurance-icon.png';
 
 class ServiceSection extends Component {
